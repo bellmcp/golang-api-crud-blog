750cf9ce9f5f0b831995ce9fd01720c425ec98dc
wutipat.k <wutipat.k@linecorp.com>
2023-08-01 12:50:13 +0700

    authorization

--- seed/seed.go
+++ seed/seed.go
@@ -15,9 +15,44 @@ func Load() {
 	db := config.GetDB()
 
 	// Clean database
-	db.DropTableIfExists("articles", "categories", "migrations")
+	db.DropTableIfExists("users", "articles", "categories", "migrations")
 
 	migrations.Migrate()
+
+	// Add Admin
+	log.Print("Creating admin...")
+
+	admin := models.User{
+		Email:    "admin@babelcoder.com",
+		Password: "passw0rd",
+		Name:     "Admin",
+		Role:     "Admin",
+		Avatar:   "https://i.pravatar.cc/100",
+	}
+
+	admin.Password = admin.GenerateEncryptedPassword()
+	db.Create(&admin)
+
+	// Add normal users
+	log.Print("Creating users...")
+
+	numOfUsers := 50
+	users := make([]models.User, 0, numOfUsers)
+	userRoles := [2]string{"Editor", "Member"}
+
+	for i := 1; i <= numOfUsers; i++ {
+		user := models.User{
+			Name:     faker.Name(),
+			Email:    faker.Email(),
+			Password: "passw0rd",
+			Avatar:   "https://i.pravatar.cc/100?" + strconv.Itoa(i),
+			Role:     userRoles[rand.Intn(2)],
+		}
+
+		user.Password = user.GenerateEncryptedPassword()
+		db.Create(&user)
+		users = append(users, user)
+	}
 
 	// Add categories
 	log.Print("Creating categories")
@@ -47,6 +82,7 @@ func Load() {
 			Body:       faker.Paragraph(),
 			Image:      "https://source.unsplash.com/random/300x300?" + strconv.Itoa(i),
 			CategoryID: uint(rand.Intn(numOfArticles) + 1),
+			UserID:     uint(rand.Intn(numOfUsers) + 1),
 		}
 
 		db.Create(&article)
